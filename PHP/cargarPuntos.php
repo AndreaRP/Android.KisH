3a44c7d3a920d408e247b91bb0e1e7650e9ebd6c
Andrea <andrea.rubiop88@gmail.com>
2015-11-18 18:34:36 +0100

    Update cargarPuntos.php

--- PHP/cargarPuntos.php
+++ PHP/cargarPuntos.php
@@ -1,12 +1,5 @@
 <?php 
-//include 'comprobarLogin.php';
-//include 'Conexion.php';
-//if (isset($_POST['campoLogin']) && $_POST['campoLogin'] != "" && //isset($_POST['campoPass']) && $_POST['campoPass'] != ""){ 
-  //  $login = $_POST['campoLogin']; 
- 	//$pass = $_POST['campoPass'];
-//   $correcto = comprobarLogin($login, $pass);
-//if($correcto){
-    $con = mysqli_connect("sql100.byethost6.com","b6_16108246","thebeatles1", "b6_16108246_KishBD"); 
+    $con = mysqli_connect("********","***********","**********", "************"); 
  	#Se comprueba la conexion
  	if (mysqli_connect_errno()) {
  	 echo 'Error de conexion: '.mysqli_connect_error(); 
@@ -21,11 +14,5 @@
         $puntos = mysqli_fetch_all($losPuntos, MYSQLI_ASSOC);
  	 	#Sacamos el resultado en formato json 
  	 	echo json_encode($puntos);  	  
- //	}else{
-   //   echo "Login o Pass incorrectos";   
-     //} 
-//}else{
-  //  echo "Error de conexion, faltan parametros"
-//}
 mysqli_close($con);
  	 	?>
