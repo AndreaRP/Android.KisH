c13a41e6921668884b699ecce607b715b1661e55
Andrea <andrea.rubiop88@gmail.com>
2015-11-18 18:33:20 +0100

    Update cargarBichoZona.php

--- PHP/cargarBichoZona.php
+++ PHP/cargarBichoZona.php
@@ -1,19 +1,11 @@
 <?php 
-//include 'comprobarLogin.php';
-//include 'Conexion.php';
-//if (isset($_POST['campoLogin']) && $_POST['campoLogin'] != "" && isset($_POST['campoPass']) && $_POST['campoPass'] != ""){ 
-//  $login = $_POST['campoLogin']; 
-// 	$pass = $_POST['campoPass'];
-//  $correcto = comprobarLogin($login, $pass);
-//if($correcto){
-    $con = mysqli_connect("sql100.byethost6.com","b6_16108246","thebeatles1", "b6_16108246_KishBD"); 
+    $con = mysqli_connect("*********","********","**********", "***********"); 
  	#Se comprueba la conexion
  	if (mysqli_connect_errno()) {
  	 echo 'Error de conexion: '.mysqli_connect_error(); 
  	 exit(); 
  	} 
  	$zonaActual = $_POST['zonaActual'];
-    //$zonaActual = '6';
     $losBichosxZona = mysqli_query($con, "select bicho_fk as 'nPokemon', Bicho.nombre as 'nombreGenerico' 
 from BichoXZona 
 inner join Bicho
@@ -22,11 +14,5 @@ where zona_fk = '".$zonaActual."'");
         $bichosZona = mysqli_fetch_all($losBichosxZona, MYSQLI_ASSOC);
  	 	#Sacamos el resultado en formato json 
  	 	echo json_encode($bichosZona);  	  
- 	//}else{
-      //echo "Login o Pass incorrectos";   
-     //} 
-//}else{
-//    echo "Error de conexion, faltan parametros"
-//}
   mysqli_close($con);
  	 	?>
