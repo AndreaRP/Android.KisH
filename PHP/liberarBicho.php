18a1046e9be54ca8e40116b0bf23a56a22f2be98
Andrea <andrea.rubiop88@gmail.com>
2015-11-18 18:37:26 +0100

    Update liberarBicho.php

--- PHP/liberarBicho.php
+++ PHP/liberarBicho.php
@@ -1,15 +1,13 @@
 <?php
 $bichos = $_POST['bicho']; 
 if (isset($bichos) && $bichos != ""){ 
-$servername = "sql100.byethost6.com";
-$username = "b6_16108246";
-$password = "thebeatles1";
-$dbname = "b6_16108246_KishBD";
+$servername = "*********";
+$username = "***********";
+$password = "**********";
+$dbname = "***********";
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
-//$login = mysqli_real_escape_string($conn, $login);
-//$bichos = mysqli_real_escape_string($conn, $bichos);
-// Check connection
+
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
     exit();
